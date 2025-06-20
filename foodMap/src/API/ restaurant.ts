d804ae394735f88172de3ff45f92ec55b58eef48
강성훈 <kshevl123@gmail.com>
2025-06-20 18:49:21 +0900

    feat: 대전 음식점 api 추가

---
+++ foodMap/src/API/ restaurant.ts
@@ -0,0 +1,13 @@
+export const fetchData = async (page: number) => {
+
+    try {
+      const res = await fetch(`/api/api/stores/?format=json&page=${page}`);
+      const data = await res.json();
+      console.log(data);
+      return data.results;
+    } catch (err) {
+      console.error("fetch 실패:", err);
+      return [];
+    }
+  
+  };
