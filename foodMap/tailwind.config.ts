4d5adcd159ec4c936fdab585e5d0b789e6e386d1
강성훈 <kshevl123@gmail.com>
2025-09-08 17:19:36 +0900

    chore: tailwind.config.ts 파일 추가

---
+++ foodMap/tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      padding: {
+        'safe-top': 'env(safe-area-inset-top)',
+        'safe-bottom': 'env(safe-area-inset-bottom)',
+        'safe-left': 'env(safe-area-inset-left)',
+        'safe-right': 'env(safe-area-inset-right)',
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
