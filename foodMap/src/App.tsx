541fd5d1a1327b48eee08cfde22c0fb6ebda5d64
강성훈 <kshevl123@gmail.com>
2025-09-08 17:48:06 +0900

    feat: Route추가 및 day.js 플러그인 추가

--- foodMap/src/App.tsx
+++ foodMap/src/App.tsx
@@ -1,94 +1,50 @@
-import { useEffect, useState } from "react";
-import { fetchData } from "./API/ restaurant";
-import useRestaurantStore from "./store/useRestaurantStore";
-import useKakaoMap from "./kakaoMap/useKakaoMap";
-import useZoomControl from "./kakaoMap/useZoomControl";
-import useMarker from "./kakaoMap/useMarker";
-import type { RestaurantType } from "./store/useRestaurantStore";
-import Kakaosummary from "./kakaoMap/KakaoSummary";
-import MapCustomOverlay from "./kakaoMap/MapCustomOverlay";
+import { Suspense } from 'react';
+import dayjs from 'dayjs';
+import isoWeek from 'dayjs/plugin/isoWeek';
+import utc from 'dayjs/plugin/utc';
+import weekday from 'dayjs/plugin/weekday';
+import weekOfYear from 'dayjs/plugin/weekOfYear';
+import { Routes, Route } from 'react-router';
+import LoadingSpinner from './components/LoadingSpinner';
+import Pagination from './page/DeajeonRestuarntsMap/components/Pagination';
+import Error from './page/error';
+import MainPage from '@/page';
+import DeajeonRestuarntsMap from '@/page/DeajeonRestuarntsMap';
+import Football from '@/page/Football/intdex';
+import StudyTimer from '@/page/StudyTimer';
+import 'dayjs/locale/ko';
 
+dayjs.locale('ko');
+dayjs.extend(weekday);
+dayjs.extend(isoWeek);
+dayjs.extend(weekOfYear);
+dayjs.extend(utc);
 
 function App() {
-  const { restaurantsData, selectedRestaurant, setRestaurantsData, setSelectedRestaurant } = useRestaurantStore();
-  const [page, setPage] = useState<number>(1);
-
-  const map = useKakaoMap();
-
-  useZoomControl(map);
-  useMarker({ map, restaurants: restaurantsData });
-
-  const handleClickPageUp = () => {
-    setSelectedRestaurant(null);
-    if (map) {
-      map.setCenter(new kakao.maps.LatLng(36.350439, 127.384950));
-      map.setLevel(7);
-    }
-    setPage(page + 1);
-  }
-
-  const handleClickRestaurant = (restaurant: RestaurantType) => {
-    setSelectedRestaurant(restaurant);
-
-    if (map && restaurant) {
-      const moveLatLon = new kakao.maps.LatLng(
-        Number(restaurant.LAT),
-        Number(restaurant.LOT)
-      );
-      map.panTo(moveLatLon);
-    }
-  }
-
-  const handleCloseOverlay = () => {
-    setSelectedRestaurant(null);
-  };
-
-  useEffect(() => {
-    const getData = async () => {
-      const data = await fetchData(page);
-      setRestaurantsData(data);
-    };
-    getData();
-  }, [page, setRestaurantsData]);
-
-
   return (
-    <div className='w-full h-full flex justify-between'>
-      <div className="w-120">
-        <div className='mb-20 justify-self-center m-10 text-3xl w-auto'>
-          대전광역시 음식점 지도
-        </div>
-        <button className="bg-gray-300 rounded-lg p-2 border border-gray-400 active:bg-gray-400"
-          onClick={handleClickPageUp}>page up</button>
-        <div className="w-auto flex flex-col gap-2 h-162 overflow-scroll border p-5 shadow-xl">
-          {restaurantsData.map((restaurant) => (
-            <div key={restaurant.REST_ID}
-              className="border border-gray-400 border-2 p-2 cursor-pointer hover:bg-neutral-200 active:bg-neutral-300 rounded-lg shadow-lg"
-              onClick={() => handleClickRestaurant(restaurant)}
-            >
-              <div className="justify-self-center">{restaurant.REST_NM} ({restaurant.TOB_INFO})</div>
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className='border border-neutral-300 shadow-2xl border-4 w-300 h-full'>
-        <div id="map" className="w-full h-full z-0" />
-        {map && selectedRestaurant ? (
-          <MapCustomOverlay
-            map={map}
-            position={new kakao.maps.LatLng(
-              Number(selectedRestaurant.LAT),
-              Number(selectedRestaurant.LOT)
-            )}
-          >
-            <Kakaosummary
-              onClose={handleCloseOverlay}
-            />
-          </MapCustomOverlay>
-        ) : null}
-      </div>
-    </div >
-  )
+    <Suspense fallback={<LoadingSpinner />}>
+      <Routes>
+        <Route path="/" element={<MainPage />}>
+          메인
+        </Route>
+        <Route path="/DeajeonRestuarntsMap" element={<DeajeonRestuarntsMap />}>
+          지도
+        </Route>
+        <Route path="/StudyTimer" element={<StudyTimer />}>
+          공부타이머
+        </Route>
+        <Route path="/football" element={<Football />}>
+          축구일정
+        </Route>
+        <Route path="*" element={<Error />}>
+          없는 페이지
+        </Route>
+        <Route path="/pagination" element={<Pagination />}>
+          페이지네이션
+        </Route>
+      </Routes>
+    </Suspense>
+  );
 }
 
 export default App;
