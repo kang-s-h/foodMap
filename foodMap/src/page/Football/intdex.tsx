e90de22d763c523febe84dc51f4187f676cf3674
강성훈 <kshevl123@gmail.com>
2025-09-08 19:01:24 +0900

    feat: 축구 캘린더 진입점 추가 및 loadingSpinner 추가

---
+++ foodMap/src/page/Football/intdex.tsx
@@ -0,0 +1,11 @@
+import { Suspense } from 'react';
+import LoadingSpinner from '@/components/LoadingSpinner';
+import FootballPage from '@/page/Football/components/football';
+
+export default function Football() {
+  return (
+    <Suspense fallback={<LoadingSpinner />}>
+      <FootballPage />
+    </Suspense>
+  );
+}
