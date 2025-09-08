1f69dc8b1d784cdf07dbfb62569667408c885bc1
강성훈 <kshevl123@gmail.com>
2025-09-08 18:50:41 +0900

    refactor: restaurant 전역 상태 스토어 수정

--- foodMap/src/store/useRestaurantStore.ts
+++ foodMap/src/store/useRestaurantStore.ts
@@ -1,39 +1,24 @@
-import { create } from "zustand";
-
-export interface RestaurantType {
-  ADDR: string;
-  LAT: string;
-  LOT: string;
-  MENU_AMT: string;
-  MENU_MENU_ADD_INFO: string;
-  OPEN_HR_INFO: string;
-  REST_NM: string;
-  TELNO: string;
-  TOB_INFO: string;
-  REST_ID: string;
-  SD_URL: string;
-}
+import { create } from 'zustand';
+import type { Restaurant } from '@/API/foodData/entity';
 
 export interface StoreStateType {
-  restaurantsData: RestaurantType[];
-  selectedRestaurant: RestaurantType | null;
-
+  currentPage: number;
+  selectedRestaurant: Restaurant | null;
 }
 
-interface StoreActionType{
-  setRestaurantsData: (data: RestaurantType[]) => void;
-  setSelectedRestaurant: (restaurantData: RestaurantType | null) => void;
+interface StoreActionType {
+  setCurrentPage: (page: number) => void;
+  setSelectedRestaurant: (restaurantData: Restaurant | null) => void;
   clearSelectedRestaurant: () => void;
-
 }
 
 const useRestaurantStore = create<StoreStateType & StoreActionType>((set) => ({
-  restaurantsData: [],
+  currentPage: 1,
   selectedRestaurant: null,
-  setRestaurantsData : (data) => set({restaurantsData: data}),
-  setSelectedRestaurant: (restaurantData) => set({ selectedRestaurant: restaurantData }),
+  setCurrentPage: (page: number) => set({ currentPage: page }),
+  setSelectedRestaurant: (restaurantData: Restaurant | null) =>
+    set({ selectedRestaurant: restaurantData }),
   clearSelectedRestaurant: () => set({ selectedRestaurant: null }),
-})) 
-
+}));
 
 export default useRestaurantStore;
